b49c32d51bebb1f3a1ec9855e6857bad5aebb9ff
Maxime Gauduin <alucryd@archlinux.org>
2020-09-24 16:24:51 +0200

    add openid connect support

---
+++ master/auth.py
@@ -0,0 +1,32 @@
+import requests
+from buildbot.www.oauth2 import OAuth2Auth
+
+
+class OpenIDAuth(OAuth2Auth):
+    name = "OpenID"
+    faIcon = "fa-openid"
+    authUriAdditionalParams = {"scope": "openid"}
+
+    def __init__(self, configurationUri, clientId, clientSecret, **kwargs):
+        r = requests.get(configurationUri)
+        r.raise_for_status()
+
+        json = r.json()
+
+        self.authUri = json["authorization_endpoint"]
+        self.tokenUri = json["token_endpoint"]
+        self.userInfoUri = json["userinfo_endpoint"]
+        super(OpenIDAuth, self).__init__(clientId, clientSecret, **kwargs)
+
+    def createSessionFromToken(self, token):
+        s = requests.Session()
+        s.headers = {"Authorization": f"Bearer {token['access_token']}"}
+        return s
+
+    def getUserInfoFromOAuthClient(self, session):
+        r = session.get(self.userInfoUri)
+        r.raise_for_status()
+
+        json = r.json()
+
+        return {"full_name": json["name"], "email": json["email"]}
